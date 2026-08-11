dbc46509149fb5bf88249da661c8d47b6acb41a8
a133-a11y <13395037144@163.com>
2026-08-11 09:59:02 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js - 占位文件，真实内容由 GitHub Actions 在部署时写入
+const CONFIG = {
+    GITHUB_TOKEN: 'PLACEHOLDER_TOKEN',
+    GITHUB_USER: 'a133-a11y',
+    GITHUB_REPO: 'a133-a11y.github.io',
+    USERS_FILE: 'users.json',
+    POSTS_FILE: 'posts.json',
+    SECRET_KEY: 'PLACEHOLDER_KEY'
+};
